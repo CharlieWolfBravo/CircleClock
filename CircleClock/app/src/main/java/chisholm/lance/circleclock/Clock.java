2cb258f9af3faca580342436afd77431469302cb
Chisholm <lance.chisholm@uleth.ca>
2018-02-15 16:36:21 -0700

    First Commit

---
+++ CircleClock/app/src/main/java/chisholm/lance/circleclock/Clock.java
@@ -0,0 +1,13 @@
+package chisholm.lance.circleclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
